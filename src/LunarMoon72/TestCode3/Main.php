9af77e94ebf7740d072208f03e54e039a97fc3bc
LunarMoon72 <88513736+LunarMoon72@users.noreply.github.com>
2021-08-06 14:28:10 -0400

    e

--- src/LunarMoon72/TestCode3/Main.php
+++ src/LunarMoon72/TestCode3/Main.php
@@ -24,23 +24,23 @@ class Main extends PluginBase implements Listener{
    public function onDisabled(){
    	$this->getLogger()->info("Plugin is Disabled");
    }
-   public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args, Player $player) : bool{
-   	switch($cmd->getName()){
-   		case "test":
-   		 if($player->hasPermission("testcode4.cmd")){
-   		 	if(!sender instanceof Player){
-   		 		$sender->sendMessage("This command is for players!");
-   		 	}else{
-   		 		if(!isset($args[0]) or (is_int($args[0]) > 0)){
-   		 			$args[0] = 4;
-   		 		}
-   		 		$sender->getInventory()->addItem(Item::get(364,0,$args[0]));
-   		 		$sender->sendMessage("You have just recieved" .count($args[0]). " steak!");
-   		 	}  
-
-   		 }
-   		break;
-   	}
-      return true;
-   }
+  public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
+   switch($cmd->getName()){
+     case "test":
+       if($sender->hasPermission("test.cmd")){
+         if(!$sender instanceof Player){
+           $sender->sendMessage("This Command Only Works for players! Please perform this command IN GAME!");
+         }else{
+           if(!isset($args[0]) or (is_int($args[0]) and $args[0] > 0)){ 
+            $args[0] = 4; 
+           }
+           $sender->getInventory()->addItem(Item::get(364,0,$args[0]));
+           $sender->sendMessage("You have just recieved" .count($args[0]). " steak!");
+         }
+       }else{
+         $sender->sendMessage("You don't have permission to use this command");
+       }
+     break;
+  }
+  return true;
 }
