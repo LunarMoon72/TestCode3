0ab8cd82f51e8d3f3cd58b96e4f966dd20adec7f
LunarMoon72 <88513736+LunarMoon72@users.noreply.github.com>
2021-08-05 20:18:09 -0400

    Create Main.php

---
+++ src/LunarMoon72/TestCode3/Main.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace LunarMoon72;
+
+use pocketmine/plugin/PluginBase;
+
+use pocketmine/Player;
+
+use pocketmine/Server;
+
+use pocketmine/event/Listener;
+
+use pocketmine/command/Command;
+
+use pocketmine/command/CommandSender;
+
+class Main extends PluginBase{
+   public function onEnabled(){
+   	$this->getServer()->getPluginManager()->registerEvents($this,$this);
+   	$this->getLogger()->info("Plugin is Enabled");
+
+   }
+   
+   public function onDisabled(){
+   	$this->getLogger()->info("Plugin is Disabled");
+   }
+   public function onCommand(CommandSender $sender, Command $cmd, string $label array $args) : bool{
+   	switch($cmd->getName()){
+   		case "test":
+   		  $sender->sendMessage("This is a test cmd!");
+   		break;
+   	}
+    return true;
+   }
+}
